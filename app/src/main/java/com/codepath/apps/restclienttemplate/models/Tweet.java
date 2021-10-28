f4e1a23ba4f1c50bb07c759c7c2706a0915c148d
futurebound <mitchellm1618@gmail.com>
2021-10-27 21:24:47 -0700

    added relative time functionality, all requried stories complete

--- app/src/main/java/com/codepath/apps/restclienttemplate/models/Tweet.java
+++ app/src/main/java/com/codepath/apps/restclienttemplate/models/Tweet.java
@@ -1,5 +1,7 @@
 package com.codepath.apps.restclienttemplate.models;
 
+import com.codepath.apps.restclienttemplate.TimeFormatter;
+
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -12,12 +14,17 @@ public class Tweet {
     public String body;
     public String createdAt;
     public User user;
+    public String relativeTime;
 
     public static Tweet fromJson(JSONObject jsonObject) throws JSONException {
         Tweet tweet = new Tweet();
         tweet.body = jsonObject.getString("text");
         tweet.createdAt = jsonObject.getString("created_at");
         tweet.user = User.fromJson(jsonObject.getJSONObject("user"));
+
+        String unformattedTime = jsonObject.getString("created_at");
+        tweet.relativeTime = TimeFormatter.getTimeDifference(unformattedTime);
+        
         return tweet;
     }
 
